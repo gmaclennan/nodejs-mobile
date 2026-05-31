a092393cc7807e9a1d9df78a17da29427419fa3a
Gregor MacLennan <gmaclennan@awana.digital>
2026-05-31 18:55:34 +0100

    src: mobile platform guards in node.cc / node_internals.h
    TARGET_OS_IPHONE / __ANDROID__ guards disabling process/dlopen paths unavailable on mobile.
    Co-authored-by: Andre Staltz <andre@staltz.com>
    Co-authored-by: Julia Samól <j.samol@papers.ch>
    Co-authored-by: Jaime Bernardo <jaime@janeasystems.com>

--- src/node.cc
+++ src/node.cc
@@ -693,6 +693,8 @@ void ResetStdio() {
       CHECK_NE(err, -1);
     }
 
+#if !(defined(__APPLE__) && TARGET_OS_IPHONE)
+    // tcsetattr is not working on iOS devices.
     if (s.isatty) {
       sigset_t sa;
       int err;
@@ -714,6 +716,7 @@ void ResetStdio() {
       // - if the process group is orphaned, e.g. because the user logged out,
       //   tcsetattr fails with EIO
     }
+#endif  // !(defined(__APPLE__) && TARGET_OS_IPHONE)
   }
 #endif  // __POSIX__
 }
